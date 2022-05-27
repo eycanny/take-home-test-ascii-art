3b38bddd50e55837ddc280b3d7a34e0756d28384
eycanny <nancy.the.coder@gmail.com>
2022-05-26 17:54:04 -0700

    Add assumptions and start functions
    - Initial completion of check_if_out_of_bounds
    - Add docstring for check_for_overlap
    - Start render

--- ascii-art-take-home.py
+++ ascii-art-take-home.py
@@ -16,10 +16,17 @@ class Canvas:
     def render(self):
         """Print out the canvas and its shapes."""
 
+        #check if any shapes are out of bounds
+        i = 0
+
+        while i < len(self.shapes):
+            self.shapes[i] = check_if_out_of_bounds(self.shapes[i], self.shapes[i+1])
+            i += 1
+
         # check if any of the shapes are overlapping
             # most recent shape should be on top
 
-        print(self)
+        # print(self)
 
     def clear_shapes(self):
         """Clear all shapes from the canvas."""
@@ -58,8 +65,11 @@ class Rectangle:
 def check_for_overlap(shape1, shape2):
     """Check if two shapes are overlapping.
 
-    If so, most recent shape's fill should overwrite older shape's fill
-    where they overlap."""
+    First parameter represents older shape. Second parameter
+    represents newer shape.
+
+    Newer shape's fill should overwrite older shape's fill where
+    they overlap."""
 
     pass
 
@@ -67,9 +77,43 @@ def check_for_overlap(shape1, shape2):
 def check_if_out_of_bounds(canvas, shape):
     """Check if a shape is out of bounds."""
 
-    pass
+    if shape.start_x < 0:
+        shape.start_x = 0
+
+    if shape.end_x < 0:
+        shape.end_x = 0
+    
+    if shape.start_y < 0:
+        shape.start_y = 0
+
+    if shape.end_y < 0:
+        shape.end_y = 0
+    
+    if shape.end_x > canvas.width:
+        shape.end_x = canvas.width
+    
+    if shape.end_y > canvas.height:
+        shape.end_y = canvas.height
+
+    return shape
 
 
 #----------------------------------------------------------------------#
 #----------------------- Assumptions / Decisions ----------------------#
 #----------------------------------------------------------------------#
+
+# Should I check the shape if it's out of bounds when adding it to the
+# canvas object or upon render?
+    # Upon render in case we add the ability to edit canvas size in the
+    # future.
+
+# I assume the height and width of the canvas starts at zero for both
+# the x- and y-axis.
+
+# I assume that it is possible for a rectangle to start and end with
+# negative axis points.
+
+# Need to somehow combine all elements into one element upon printing.
+    # How to print blank canvas
+        # for i in range of (height)
+            # print space (width) times
